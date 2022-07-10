f0d8c26a9fbc4277e27bacb0df2675f913017025
The Lood <unilood@gmail.com>
2022-07-10 12:57:52 +1200

    Exercise 5: Scrabble score(update)

--- src/main/kotlin/ScrabbleScore.kt
+++ src/main/kotlin/ScrabbleScore.kt
@@ -1,41 +1,21 @@
 object ScrabbleScore {
 
-    fun scoreLetter(c: Char): Int {
-        val letterScore = mapOf(
-            'A' to 1,
-            'E' to 1,
-            'I' to 1,
-            'O' to 1,
-            'U' to 1,
-            'L' to 1,
-            'N' to 1,
-            'R' to 1,
-            'S' to 1,
-            'T' to 1,
-            'D' to 2,
-            'G' to 2,
-            'B' to 3,
-            'C' to 3,
-            'M' to 3,
-            'P' to 3,
-            'F' to 4,
-            'H' to 4,
-            'V' to 4,
-            'W' to 4,
-            'Y' to 4,
-            'K' to 5,
-            'J' to 8,
-            'X' to 8,
-            'Q' to 10,
-            'Z' to 10
-        )
-        return letterScore[c]!!
+    private fun scoreLetter(c: Char): Int {
+        return when (c) {
+            in "aeioulnrst" -> 1
+            in "dg" -> 2
+            in "bcmp" -> 3
+            in "fhvwy" -> 4
+            in "k" -> 5
+            in "jx" -> 8
+            in "qz" -> 10
+            else -> 0
+        }
     }
 
     fun scoreWord(word: String): Int {
         var score = 0
-        val upperWord: CharArray = word.uppercase().toCharArray()
-        for (c: Char in upperWord) {
+        for (c: Char in word.lowercase().toCharArray()) {
             score += scoreLetter(c)
         }
         return score
