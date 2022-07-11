7b5f4667188a2125d9322d80c675169b671d25fb
The Lood <unilood@gmail.com>
2022-07-12 08:37:47 +1200

    Exercise 8: Secret Handshake(update)

--- src/main/kotlin/HandshakeCalculator.kt
+++ src/main/kotlin/HandshakeCalculator.kt
@@ -1,11 +1,8 @@
-import helper.Signal
-
 object HandshakeCalculator {
-    private val signals : MutableMap<Int, Signal> = mutableMapOf()
+    private val signals : MutableMap<Int, Signal> = setupSignals()
     fun calculateHandshake(number: Int): List<Signal> {
         val handshake: MutableList<Signal> = mutableListOf()
         var keys: MutableSet<Int> = signals.keys
-        setupSignals()
         var binary: Int = Integer.parseInt(Integer.toBinaryString(number))
             while (binary >= 10000) {
                 binary -= 10000
@@ -26,10 +23,11 @@ object HandshakeCalculator {
         return handshake
     }
 
-    private fun setupSignals() {
-        signals[1] = Signal.WINK
-        signals[10] = Signal.DOUBLE_BLINK
-        signals[100] = Signal.CLOSE_YOUR_EYES
-        signals[1000] = Signal.JUMP
+    private fun setupSignals(): MutableMap<Int, Signal> {
+        return mutableMapOf(
+        1 to Signal.WINK,
+        10 to Signal.DOUBLE_BLINK,
+        100 to Signal.CLOSE_YOUR_EYES,
+        1000 to Signal.JUMP)
     }
 }
