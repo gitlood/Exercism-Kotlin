7d2a0367bef6207078bc1cedaf06da4c8715db79
The Lood <unilood@gmail.com>
2022-07-13 10:37:20 +1200

    Secret Handshake update

--- src/main/kotlin/HandshakeCalculator.kt
+++ src/main/kotlin/HandshakeCalculator.kt
@@ -1,28 +1,23 @@
 import java.lang.Integer.toBinaryString
 
 object HandshakeCalculator {
-    private val signals = setupSignals()
     fun calculateHandshake(number: Int): List<Signal> {
         val handshake: MutableList<Signal> = mutableListOf()
-        var binary = toBinaryString(number).toCharArray().reversed()
-        var reverse = false
-        if (binary.size > 4) {
-            reverse = true
-            binary = binary.subList(0, 4)
-        }
-        for (i in binary.indices) {
-            if (binary[i] == '1') {
-                handshake.add(signals[i])
+        val binary = toBinaryString(number).toCharArray().reversed()
+
+        binary.indices.forEach {
+            if (it < 4 && binary[it] == '1') {
+                handshake.add(signals()[it])
             }
         }
-        return if (reverse) {
+        return if (binary.size > 4) {
             handshake.reversed()
         } else {
             handshake
         }
     }
 
-    private fun setupSignals(): List<Signal> {
+    private fun signals(): List<Signal> {
         return listOf(
             Signal.WINK,
             Signal.DOUBLE_BLINK,
