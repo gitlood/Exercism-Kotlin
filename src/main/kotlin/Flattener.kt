daa8e38bf68fda8422fe265907f1b01d4a6c52d9
The Lood <unilood@gmail.com>
2022-07-19 11:27:40 +1200

    Flattener update

--- src/main/kotlin/Flattener.kt
+++ src/main/kotlin/Flattener.kt
@@ -1,36 +1,20 @@
 object Flattener {
+    var flattenedSource: MutableList<Int> = mutableListOf()
 
     fun flatten(source: Collection<Any?>): List<Any> {
-        return convertToMap(source.filterNotNull())
+        flattenedSource = mutableListOf()
+        flattenList(source.toList())
+        return flattenedSource
     }
 
-    private fun convertToMap(theSource: Collection<Any>): MutableList<Int> {
-        val theSourceMap: MutableList<Int> = mutableListOf()
-        theSource.forEach { item ->
-            if (item is Int) {
-                theSourceMap.add(item)
-            }
-            if (item is List<*>) {
-                item.filterNotNull().forEach { firstLevelInt ->
-                    if (firstLevelInt is Int) {
-                        theSourceMap.add(firstLevelInt)
-                    } else if (firstLevelInt is List<*>) {
-                        firstLevelInt.forEach { secondLevelInt ->
-                            if (secondLevelInt is Int) {
-                                theSourceMap.add(secondLevelInt)
-                            } else if (secondLevelInt is List<*>) {
-                                secondLevelInt.forEach { thirdLevelInt ->
-                                    if (thirdLevelInt is Int) {
-                                        theSourceMap.add(thirdLevelInt)
-                                    } else if (thirdLevelInt is List<*>)
-                                        theSourceMap.add(thirdLevelInt[0] as Int)
-                                }
-                            }
-                        }
-                    }
-                }
+    private fun flattenList(item: List<*>) : List<Any>{
+        item.filterNotNull().forEach { intOrList ->
+            if (intOrList is Int) {
+                flattenedSource.add(intOrList)
+            }else {
+                flattenList(intOrList as List<*>)
             }
         }
-        return theSourceMap
+        return flattenedSource
     }
 }
